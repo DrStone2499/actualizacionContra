5f5ccd1c8944b823f0bab3ae2accea591a5baeb1
DrStone2499 <57408004+DrStone2499@users.noreply.github.com>
2020-06-10 18:28:54 -0600

    Add files via upload

---
+++ Login2.1/src/main/java/com/dsapimx/login/back/actualizacion.java
@@ -0,0 +1,14 @@
+package com.dsapimx.login.back;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import static org.apache.commons.lang3.RandomStringUtils.*;
+
+public class actualizacion {
+
+    public static String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?";
+    public static String contra = RandomStringUtils.random( 8, characters );
+
+
+
+
+}
